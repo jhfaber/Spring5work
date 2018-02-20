91cd88508e8dbc110326d23db667bbc06a9ff4bb
jhfaber <jfmarina@unal.edu.co>
2018-02-20 09:24:54 -0500

    first commit

---
+++ src/main/java/com/spring/Spring5work/Spring5workApplication.java
@@ -0,0 +1,12 @@
+package com.spring.Spring5work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5workApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5workApplication.class, args);
+	}
+}
